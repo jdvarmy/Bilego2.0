f8ee9cbf92558e3d5ac74d65ea66d44b5c14ac19
sergey.antonov <sergey.antonov@nedra.digital>
2022-03-15 19:31:15 +0300

    добавили футер меню

--- bclient/src/components/MenuFooter/MenuFooter.tsx
+++ bclient/src/components/MenuFooter/MenuFooter.tsx
@@ -1,5 +1,8 @@
 import React, { memo } from 'react';
 import Link from 'next/link';
+import IconTelegram from './IconTelegram';
+import IconVk from './IconVk';
+import IconInstagram from './IconInstagram';
 
 const MenuFooter = () => {
   console.log('FOOTER');
@@ -7,25 +10,36 @@ const MenuFooter = () => {
   return (
     <div className='flex flex-col justify-end text-my-purple'>
       <div className='h-0 border-t border-my-purple' />
-      <div className='my-4'>
-        <div className='flex flex-row my-1'>
+      <div className='my-4 grid grid-rows-2 grid-flow-col gap-0'>
+        <div>
           <Link href={'/contacts'}>
             <a className='cursor-pointer'>контакты</a>
           </Link>
+        </div>
+        <div>
+          <Link href={'/offer'}>
+            <a className='cursor-pointer'>оферта</a>
+          </Link>
+        </div>
+        <div>
           <Link href={'/items'}>
             <a className='cursor-pointer'>места</a>
           </Link>
         </div>
-        <div className='flex flex-row my-1'>
-          <Link href={'/offer'}>
-            <a className='cursor-pointer'>оферта</a>
-          </Link>
+        <div>
           <Link href={'/about'}>
             <a className='cursor-pointer'>о нас</a>
           </Link>
         </div>
       </div>
-      <div className='mb-3'>Bilego {new Date().getFullYear()}</div>
+      <div className='mb-3 flex flex-row justify-between'>
+        <div>Bilego {new Date().getFullYear()}</div>
+        <div className='flex flex-row items-center'>
+          <IconTelegram />
+          <IconVk />
+          <IconInstagram />
+        </div>
+      </div>
     </div>
   );
 };
