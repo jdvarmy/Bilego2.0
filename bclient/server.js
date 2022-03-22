4776f08ece80f8cd20864e56a941880389d99f76
sergey.antonov <sergey.antonov@nedra.digital>
2022-03-23 02:21:09 +0300

    client * удалили next config из server.js

--- bclient/server.js
+++ bclient/server.js
@@ -1,13 +1,14 @@
 const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
-const conf = require('./next.config');
+// const conf = require('./next.config');
 
 const dev = process.env.NODE_ENV !== 'production';
 const hostname = 'localhost';
 const port = 3000;
 // when using middleware `hostname` and `port` must be provided below
-const app = next({ dev, hostname, port, conf });
+// const app = next({ dev, hostname, port, conf });
+const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
